94b1de3f93ffb698bff6a13bb1a22164be29becd
morsgiathatch <corbinwilliambaldwin@gmail.com>
2019-07-16 19:47:22 -0600

    update click handler

--- GUI/main.c
+++ GUI/main.c
@@ -25,7 +25,11 @@
 #define pink_square_j(j) (j)
 
 static bool PLAYER_IS_WHITE = false;
-static bool PIECE_CURRENTLY_CLICKED = false;
+static bool PIECE_PREVIOUSLY_CLICKED = false;
+static int  PIECE_PREVIOUSLY_CLICKED_I = 0;
+static int  PIECE_PREVIOUSLY_CLICKED_J = 0;
+
+
 
 typedef struct BoardGUI{
     Board * board;
@@ -69,11 +73,51 @@ Tile * get_piece_at(int row, int col, Board * board){
     return NULL;
 }
 
-
-GtkWidget * get_widget_at(int row, int col, BoardGUI * board_gui){
+void delete_widget_at(int row, int col, BoardGUI * board_gui, bool pink_squares){
     GList * children;
     GList * child_;
-    children = board_gui->pieces;
+    if (pink_squares)
+        children = board_gui->pink_squares;
+    else
+        children = board_gui->pieces;
+
+    GtkWidget * child;
+
+    for (child_ = children; child_ != NULL; child_ = child_->next){
+        int x, y;
+        child = (GtkWidget *)(child_->data);
+        if (GTK_IS_WIDGET(child)){
+            GtkAllocation allocation;
+            GtkAllocation * alloc_ptr = &allocation;
+
+            gtk_widget_get_allocation(child, alloc_ptr);
+
+            x = ((GdkRectangle *)alloc_ptr)->x;
+            y = ((GdkRectangle *)alloc_ptr)->y;
+
+            int row_, col_;
+            get_board_coords(&row_, &col_, x, y);
+            if (col_ == col && row_ == row){
+                if (pink_squares)
+                    board_gui->pink_squares = g_list_remove(board_gui->pink_squares, child);
+                else
+                    board_gui->pieces = g_list_remove(board_gui->pieces, child);
+                // remove child from board background
+                gtk_container_remove(GTK_CONTAINER(board_gui->board_background), GTK_WIDGET(child));
+            }
+        }
+    }
+    return NULL;
+
+}
+
+GtkWidget * get_widget_at(int row, int col, BoardGUI * board_gui, bool pink_squares){
+    GList * children;
+    GList * child_;
+    if (pink_squares)
+        children = board_gui->pink_squares;
+    else
+        children = board_gui->pieces;
 
     GtkWidget * child;
 
@@ -109,47 +153,88 @@ void color_possible_moves(BoardGUI * board_gui, char * buffer, int buffer_len){
         row = buffer[i] - '0';
         col = buffer[i + 1] - '0';
         GtkWidget * piece;
-        piece = get_widget_at(row, col, board_gui);
+        piece = get_widget_at(row, col, board_gui, false);
 
         int i_, j_;
         get_window_coords(&i_, &j_, row, col);
         GtkWidget * pink_square;
         pink_square = gtk_image_new_from_file (realpath("./imgs/pink_square.png", NULL));
-        gtk_fixed_put(board_background, pink_square, pink_square_j(j_), pink_square_i(i_));
-        board_gui->pieces = g_list_append(board_gui->pieces, pink_square);
+        board_gui->pink_squares = g_list_append(board_gui->pink_squares, pink_square);
 
-        if (piece != NULL){
-            gtk_fixed_move(board_background, piece, j_, i_);
+        if (piece == NULL){
+            gtk_fixed_put(board_background, pink_square, pink_square_j(j_), pink_square_i(i_));
         }
+        else {
+            GtkWidget * overlay;
+            overlay = gtk_fixed_new();
+            gtk_fixed_put(GTK_FIXED(overlay), GTK_WIDGET(pink_square), 0, 0);
 
+            Tile * tile = get_piece_at(row, col, board_gui->board);
+            GtkWidget * piece_img;
+            piece_img = gtk_image_new_from_file(realpath(tile->path_to_img, NULL));
+            gtk_fixed_put(GTK_WIDGET(overlay), piece_img, 0, 2);
+            gtk_fixed_put(board_background, GTK_WIDGET(overlay), pink_square_j(j_), pink_square_i(i_));
+            board_gui->pink_squares = g_list_append(board_gui->pink_squares, overlay);
+
+            gtk_widget_show_all(overlay);
+        }
     }
-
     gtk_widget_show_all(GTK_WIDGET(board_background));
 }
 
 
 gboolean piece_clicked(GtkWidget *window, GdkEvent * event, gpointer data){
-    if (PIECE_CURRENTLY_CLICKED){
-        // Need to remove all pink squares if click is on pink square. Then update piece position.
+    BoardGUI * board_gui = (BoardGUI *)data;
+    Board * board = board_gui->board;
+
+    gdouble x;
+    gdouble y;
+    gdk_event_get_coords(event, &x, &y);
+    // find x_, y_ coords in board
+    int row, col;
+    if (PLAYER_IS_WHITE){
+        row = y_img_coord_to_white_row(y);
+        col = x_img_coord_to_white_col(x);
+    }
+    else{
+        row = y_img_coord_to_black_row(y);
+        col = x_img_coord_to_black_col(x);
+    }
+
+
+    if (PIECE_PREVIOUSLY_CLICKED){
+        GtkWidget * clicked_square;
+        clicked_square = get_widget_at(row, col, board_gui, true);
+        // If user clicked on a pink square
+        if (clicked_square != NULL){
+            GtkWidget * originally_clicked_piece, * target_tile;
+            originally_clicked_piece = get_widget_at(PIECE_PREVIOUSLY_CLICKED_I, PIECE_PREVIOUSLY_CLICKED_J, board_gui, false);
+            target_tile = get_widget_at(row, col, board_gui, false);
+            // delete existing piece at target tile, if existent
+            if (target_tile != NULL)
+                delete_widget_at(row, col, board_gui, false);
+
+            int x_coord, y_coord;
+            get_window_coords(&y_coord, &x_coord, row, col);
+            gtk_fixed_move(GTK_FIXED(board_gui->board_background), originally_clicked_piece, x_coord, y_coord);
+            printf("%d,%d!!\n", row, col);
+            // Update board state internals, but I don't think I need these anymore
+//            board_gui->board->pieces[PIECE_PREVIOUSLY_CLICKED_I]
+        }
+
+        // Remove all pink squares
+        GList * l;
+        for (l = board_gui->pink_squares; l != NULL; l = l->next)
+            gtk_container_remove(GTK_CONTAINER(board_gui->board_background), GTK_WIDGET(l->data));
+        g_list_free(board_gui->pink_squares);
+        board_gui->pink_squares = NULL;
+        PIECE_PREVIOUSLY_CLICKED = false;
+        gtk_widget_show_all(GTK_WIDGET(board_gui->window));
+
+        // Now wait for python to make opponent move
+
         return true;
     }else{
-        BoardGUI * board_gui = (BoardGUI *)data;
-        Board * board = board_gui->board;
-
-        gdouble x;
-        gdouble y;
-        gdk_event_get_coords(event, &x, &y);
-        // find x_, y_ coords in board
-        int row, col;
-        if (PLAYER_IS_WHITE){
-            row = y_img_coord_to_white_row(y);
-            col = x_img_coord_to_white_col(x);
-        }
-        else{
-            row = y_img_coord_to_black_row(y);
-            col = x_img_coord_to_black_col(x);
-        }
-
         // Find piece at click
         Tile * piece;
         piece = NULL;
@@ -171,7 +256,9 @@ gboolean piece_clicked(GtkWidget *window, GdkEvent * event, gpointer data){
         buffer_len = strlen(buffer);
         // read each move and update color of board.
         color_possible_moves(board_gui, buffer, buffer_len - 1);
-        PIECE_CURRENTLY_CLICKED = true;
+        PIECE_PREVIOUSLY_CLICKED = true;
+        PIECE_PREVIOUSLY_CLICKED_I = row;
+        PIECE_PREVIOUSLY_CLICKED_J = col;
         // return true to stop propagating event signal
         return true;
     }
@@ -279,7 +366,6 @@ int main(int argc,char *argv[]){
 
     g_signal_connect (white_button, "clicked", G_CALLBACK (start_as_white), board_gui);
     g_signal_connect (black_button, "clicked", G_CALLBACK (start_as_black), board_gui);
-
     gtk_box_pack_start(GTK_BOX(button_panel), white_button, true, true, 20);
     gtk_box_pack_end(GTK_BOX(button_panel), black_button, true, true, 20);
     gtk_container_add(GTK_CONTAINER (window), button_panel);
@@ -287,6 +373,7 @@ int main(int argc,char *argv[]){
     gtk_widget_show_all(window);
 
     gtk_main();
+    free(board_gui);
 
     return 0;
 }
